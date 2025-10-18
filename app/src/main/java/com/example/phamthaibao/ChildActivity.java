fa5919fd65ad918b655bb8a2adb69b46951974b1
thaibao-15 <phamthaibao2005thd@gmail.com>
2025-10-18 09:06:45 +0700

    bai 3

--- app/src/main/java/com/example/phamthaibao/ChildActivity.java
+++ app/src/main/java/com/example/phamthaibao/ChildActivity.java
@@ -1,6 +1,10 @@
 package com.example.phamthaibao;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class ChildActivity extends AppCompatActivity {
+    EditText edtUsd;
+    TextView tvVnd;
+    Button btnConvert2;
+    DecimalFormat decimalFormat = new DecimalFormat("###.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,24 @@ public class ChildActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edtUsd = findViewById(R.id.edtUsd);
+        tvVnd = findViewById(R.id.tvVnd);
+        btnConvert2 = findViewById(R.id.btnConvert2);
+        Intent intent = getIntent();
+        double vnd = intent.getDoubleExtra("vnd",0);
+        double usd = vnd/26300;
+        String usdFormat= decimalFormat.format(usd);
+        tvVnd.setText("Chuyển "+ vnd +" VND thành "+usdFormat +" USD");
+        edtUsd.setText(usdFormat);
+
+        btnConvert2.setOnClickListener(v -> {
+            double textUsd= Double.parseDouble(edtUsd.getText().toString().replace(",",""));
+            double vndtext = textUsd*26300;
+            String vndFormat = decimalFormat.format(vndtext);
+            intent.putExtra("kq",vndFormat);
+            setResult(34,intent);
+            finish();
         });
     }
 }
--- app/src/main/java/com/example/phamthaibao/MainActivity.java
+++ app/src/main/java/com/example/phamthaibao/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.phamthaibao;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+    EditText edtVnd;
+    TextView tvUsd;
+    Button btnConvert1;
+    DecimalFormat decimalFormat = new DecimalFormat("###.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        edtVnd = findViewById(R.id.edtVnd);
+        tvUsd = findViewById(R.id.tvUsd);
+        btnConvert1 = findViewById(R.id.btnConcvert1);
+        btnConvert1.setOnClickListener(v -> {
+            double edtVndvalue= Double.parseDouble(edtVnd.getText().toString().trim());
+
+            Intent intent = new Intent(MainActivity.this, ChildActivity.class);
+            intent.putExtra("vnd",edtVndvalue);
+            startActivityForResult(intent,99);
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==99 && resultCode == 34){
+            String vndValue = data.getStringExtra("kq");
+            tvUsd.setText(vndValue);
+            edtVnd.setText(vndValue);
+        }
     }
 }
